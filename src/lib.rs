57b95d417720ffa0de19ee49b0283dcf06d91d4e
GabrielBernardoDaSilva <gabriel7798@gmail.com>
2024-04-11 18:12:23 -0300

    Bump version to 0.2.3 in Cargo.toml and Cargo.lock

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-pub use prime_derived::{hierarchy_ethereal_flow, DestinyRiftArcaneScript, EtherealFlowArcaneScript};
+pub use prime_derived::{
+    hierarchy_ethereal_flow, DestinyRiftArcaneScript, EtherealFlowArcaneScript,
+};
 pub use prime_forge::{
-    arcane_weft::ArcaneWeft, forged_trait::ForgedTrait, lost_realm::LostRealm, soul_thread::{EssenceAspect, SoulThread, TemporalPause}
+    arcane_weft::ArcaneWeft,
+    forged_trait::{self, ForgedHierarchy, ForgedTrait, TransformSpecialTrait},
+    lost_realm::LostRealm,
+    soul_thread::{self, EssenceAspect, SoulThread, SoulThreadManager, TemporalPause},
 };
