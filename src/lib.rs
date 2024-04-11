c9cb116d25b447542012e487b4e2bfa444b25772
GabrielBernardoDaSilva <gabriel7798@gmail.com>
2024-04-11 17:31:18 -0300

    Update dependencies and remove unused code files

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub use prime_derived::{hierarchy_ethereal_flow, DestinyRiftArcaneScript, EtherealFlowArcaneScript};
-pub use prime_forge::{
-    arcane_weft::ArcaneWeft,
-    forged_trait::ForgedTrait,
-    lost_realm::LostRealm,
-    soul_thread::{EssenceAspect, SoulThread, TemporalPause},
-};
